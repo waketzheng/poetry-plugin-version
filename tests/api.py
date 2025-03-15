f47d4dfcb72f1c030060adda3a8f26215f61b8db
Waket Zheng <waketzheng@gmail.com>
2025-03-15 10:20:03 +0800

    refactor: make it easy to start static serve

--- tests/api.py
+++ tests/api.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python
 from pathlib import Path
 
+import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 dist_dir = Path(__file__).parent.resolve().parent / "dist"
-if not dist_dir.exists():
-    dist_dir.mkdir()
 app = FastAPI()
-app.mount("/dist", StaticFiles(directory=dist_dir))
+app.mount("/dist", StaticFiles(directory=dist_dir, check_dir=False))
+
+
+if __name__ == "__main__":
+    uvicorn.run("__main__:app")
